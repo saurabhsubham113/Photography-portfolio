d151ceb33d282ab1d4ceeea9d12f2f244b876cf1
subham saurabh <saurabhsubahm113@gmail.com>
2021-01-21 23:23:44 +0530

    added footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,14 +4,12 @@ import './footer.scss'
 function Footer() {
     return (
         <footer className="footer">
-            <address>
-                307 Shiva Woods Appartment 6,
-                NGEF Layout, Naagarabhaavi Bangalore,
-                Karnataka, India 560072
-            </address>
-            <p>
-                devloped by <a target='_blank' rel="noreferrer" href="https://www.linkedin.com/in/subham-saurabh/">Subham Saurabh</a>
-            </p>
+            <div className="footer-container">
+                <p>&copy; Ankit Raj Photography 2020</p>
+                <p>
+                    devloped by <a target='_blank' rel="noreferrer" href="https://www.linkedin.com/in/subham-saurabh/">Subham Saurabh</a>
+                </p>
+            </div>
         </footer>
     )
 }
